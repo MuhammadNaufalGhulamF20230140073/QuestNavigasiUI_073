49fa20f95d847006be44a21e20d106fad83f7c6c
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-06 12:14:45 +0700

    tambahkan NavHost dengan rute Formulirku dan navigasi ke Detail

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,10 +1,13 @@
 package com.example.navigasi
 
 import android.widget.MediaController
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi{
@@ -19,6 +22,20 @@ fun DataApp(
     modifier: Modifier
 ){
     Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+                composable(route = Navigasi.Formulirku.name){
+                    FormIsian(
+                        OnSubmitBtnClick = {
+                            navController.navigate(Navigasi.Detail.name)
+                        }
+                    )
+                }
+
+                }
 
     }
 }
